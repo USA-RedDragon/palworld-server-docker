d69f035f69a8dfbf1a6ea6c5f59e3ee90633daba
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-02-27 20:18:16 -0600

    Handle shutdown and autoreboot similarly

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,13 +2,7 @@
 
 # shellcheck disable=SC2317
 term_handler() {
-    if [ "${RCON_ENABLED,,}" = true ]; then
-        rcon-cli save
-        rcon-cli "shutdown 1"
-    else # Does not save
-        kill -SIGTERM "$(pidof PalServer-Linux-Test)"
-    fi
-
+    shutdown
     tail --pid="$killpid" -f 2>/dev/null
 }
 
